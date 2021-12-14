944dc5c6cdb1f67f0f0d40e9147425944d7abeb2
NillageNakamura <nillage.tharma@gmail.com>
2021-12-14 23:59:53 +0900

    ルーティングの設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// ルートの追加。
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 
-const routes: Routes = [];
+// pathMatch は、リダイレクト先を full か prefix で指定できます。full の場合はURLが path と完全に一致、prefix の場合はURLが path の値で始まる場合にリダイレクト。
+const routes: Routes = [
+  { path: 'products', component:ProductListComponent },
+  { path: 'products/:id', component:ProductDetailComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'prefix' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
